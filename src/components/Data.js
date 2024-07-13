956f01a82aec58312054dd4beeae6551265ad182
lkdgr8 <2016latesh.kakrai@ves.ac.in>
2024-07-13 21:27:59 +0530

    updating content

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,90 +2,90 @@ const ProjectData = [
     {
         title: "Eterne Clinic Website",
         category: "UI/UX Design",
-        image: "img/eterne-website-main.png",
+        image: "eterne-website-main.png",
         link: "https://eterneclinic.com/"
     },
     {
         title: "5W&N Website",
         category: "UI/UX Design",
-        image: "img/5wn-website.jpg",
+        image: "5wn-website.jpg",
         link: "https://5wn.marketing"
     },
     {
         title: "AIKTC Web pages",    
         category: "UI/UX Design",
-        image: "img/Aiktc_landing_page.png",
+        image: "Aiktc_landing_page.png",
         link: "https://aiktc.ac.in/admission/bachelor-of-electrical-engineering-degree-course/"
     },
     {
         title: "Seaock Lounge",
         category: "Wordpress",
-        image: "img/searock.png",
+        image: "searock.png",
         link: "https://searocklounge.com/"
     },    
     {
         title: "LK Tailor Website",
         category: "Wordpress",
-        image: "img/lk-tailor.png",
+        image: "lk-tailor.png",
         link: "https://searocklounge.com/"
     },  
     {
         title: "AI Shipping Lines website",
         category: "Wordpress",
-        image: "img/ai.png",
+        image: "ai.png",
         link: "https://www.aishippinglines.com/"
     },
     {
         title: "Flatbed Hauling Qoutes website",
         category: "Wordpress",
-        image: "img/fbhq.png",
+        image: "fbhq.png",
         link: "https://www.flatbedhaulingquotes.com/"
     },
     {
         title: "AP Car Rentals website",
         category: "Wordpress",
-        image: "img/ap.png",
+        image: "ap.png",
         link: "https://apsupercarrental.com/"
     },
     {
         title: "heavy hauling transport website",
         category: "Wordpress",
-        image: "img/hht.png",
+        image: "hht.png",
         link: "https://heavyhaultransporting.com/"
     },
     {
         title: "Erols Taloring website",
         category: "UI/UX Design",
-        image: "img/erolstailoring.png",
+        image: "erolstailoring.png",
         link: "https://erolstailoring.com/"
     },
     {
         title: "KS Softech Website",    
         category: "UI/UX Design",
-        image: "img/kssoftech.png",
+        image: "kssoftech.png",
         link: "https://www.kssoftech.com/"
     },
     {
         title: "Todo List Project with edit and delete functionality",
         category: "Reactjs",
-        image: "img/todo_list_project.png",
+        image: "todo_list_project.png",
         link: "https://lkdgr8.github.io/my-todo-list-project/"
     },
     {
         title: "Robo Frinds app with data imported from API",
         category: "Reactjs",
-        image: "img/Robofrnds.png",
+        image: "Robofrnds.png",
         link: "https://lkdgr8.github.io/RoboFreindsapp/"
     },
     {
         title: "Ave portfolio website",
         category: "Reactjs",
-        image: "img/ave_project.png",
+        image: "ave_project.png",
     },
     {
         title: "weather details city wise api project",
         category: "Reactjs",
-        image: "img/weather_proj.png",
+        image: "weather_proj.png",
         link: "https://lkdgr8.github.io/weatherapiproj"
     }
 ];
