f12e4d1694a3cefe595dc59e4280d66def818704
lkdgr8 <2016latesh.kakrai@ves.ac.in>
2026-02-27 21:44:23 +0530

    Update portfolio

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,97 +1,99 @@
+const base = process.env.PUBLIC_URL;
+
 const ProjectData = [
     {
-        title: "Pragati glass Website",
+        title: "Konnect Insights Website",
         category: "Wordpress",
-        image: "img/pragati.png",
-        link: "https://designcentric.co.in/pragati/"
+        image: `${base}/img/konnectinsights.png`,
+        link: "https://konnectinsights.com/"
+    },
+    {
+        title: "Right Careers Website",
+        category: "Reactjs",
+        image: `${base}/img/rightcareers.png`,
+        link: "https://www.rightcareers.co.in/"
+    },
+    {
+        title: "Pragati Glass Website",
+        category: "Wordpress",
+        image: `${base}/img/pragati.png`,
+        link: "https://pragatiglass.com/"
     },
     {
         title: "Eterne Clinic Website",
         category: "UI/UX Design",
-        image: "img/eterne-website-main.png",
+        image: `${base}/img/eterne-website-main.png`,
         link: "https://eterneclinic.com/"
     },
     {
         title: "5W&N Website",
         category: "UI/UX Design",
-        image: "img/5wn-website.jpg",
+        image: `${base}/img/5wn-website.jpg`,
         link: "https://5wn.marketing"
     },
     {
-        title: "AIKTC Web pages",    
-        category: "UI/UX Design",
-        image: "img/Aiktc_landing_page.png",
-        link: "https://aiktc.ac.in/admission/bachelor-of-electrical-engineering-degree-course/"
-    },
-    {
-        title: "Seaock Lounge",
-        category: "Wordpress",
-        image: "img/searock.png",
-        link: "https://searocklounge.com/"
-    },    
-    {
         title: "LK Tailor Website",
         category: "Wordpress",
-        image: "img/lk-tailor.png",
-        link: "https://searocklounge.com/"
+        image: `${base}/img/lk-tailor.png`,
+        link: "https://lktailor.com/"
     },  
     {
         title: "AI Shipping Lines website",
         category: "Wordpress",
-        image: "img/ai.png",
+        image: `${base}/img/ai.png`,
         link: "https://www.aishippinglines.com/"
     },
     {
         title: "Flatbed Hauling Qoutes website",
         category: "Wordpress",
-        image: "img/fbhq.png",
+        image: `${base}/img/fbhq.png`,
         link: "https://www.flatbedhaulingquotes.com/"
     },
     {
         title: "AP Car Rentals website",
         category: "Wordpress",
-        image: "img/ap.png",
+        image: `${base}/img/ap.png`,
         link: "https://apsupercarrental.com/"
     },
     {
-        title: "heavy hauling transport website",
+        title: "Heavy Hauling Transport Website",
         category: "Wordpress",
-        image: "img/hht.png",
+        image: `${base}/img/hht.png`,
         link: "https://heavyhaultransporting.com/"
     },
     {
         title: "Erols Taloring website",
         category: "UI/UX Design",
-        image: "img/erolstailoring.png",
+        image: `${base}/img/erolstailoring.png`,
         link: "https://erolstailoring.com/"
     },
     {
         title: "KS Softech Website",    
         category: "UI/UX Design",
-        image: "img/kssoftech.png",
+        image: `${base}/img/kssoftech.png`,
         link: "https://www.kssoftech.com/"
     },
     {
         title: "Todo List Project with edit and delete functionality",
         category: "Reactjs",
-        image: "img/todo_list_project.png",
+        image: `${base}/img/todo_list_project.png`,
         link: "https://lkdgr8.github.io/my-todo-list-project/"
     },
     {
         title: "Robo Frinds app with data imported from API",
         category: "Reactjs",
-        image: "img/Robofrnds.png",
+        image: `${base}/img/Robofrnds.png`,
         link: "https://lkdgr8.github.io/RoboFreindsapp/"
     },
     {
         title: "Ave portfolio website",
         category: "Reactjs",
-        image: "img/ave_project.png",
+        image: `${base}/img/ave_project.png`,
     },
     {
         title: "weather details city wise api project",
         category: "Reactjs",
-        image: "img/weather_proj.png",
+        image: `${base}/img/weather_proj.png`,
         link: "https://lkdgr8.github.io/weatherapiproj"
     }
 ];
